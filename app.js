f7e549b9c6301a2ad7ab17f9d8c0fc54eed2ec92
Da-Hora-stack <miguelgarciadahora09@gmail.com>
2026-03-02 09:06:13 -0300

    fiz alterações e fiz o leclerc

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/leclerc", (req, res, next) => {
+    return res.status(200).sendFile("leclerc.html", { root: './' });
+});
+
+app.use((req, res, next)=> {
+    const error = new Error("Not found...");
+    error.status = 404;
+    next(error);
+});
+
+module.exports = app;
